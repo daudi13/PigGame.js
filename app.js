7fd49a6cb14b93be8a763a0dcdaca1a2abd5fcd1
daudi13 <davouma13@gmail.com>
2021-06-25 04:54:52 +0300

    commit part 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strcit';
+
+// selection of elements
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+const diceImgEl = document.querySelector('.dice');
+
+//starting defaults
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceImgEl.classList.add('hidden');
+
