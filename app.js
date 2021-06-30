c218054f4a198e8f374ff8fd2cc8f295a5226caa
daudi13 <davouma13@gmail.com>
2021-06-30 07:49:28 +0300

    commit switch player

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,7 +16,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-let currentScore  = 0
+
+const scores = [0, 0]
+let currentScore = 0;
+let activePlayer = 0;
 
 //rolling the dice function 
 
@@ -33,13 +38,17 @@ btnRoll.addEventListener('click', () => {
     if (dice !== 1) {
         //add dice to the current score
         currentScore += dice;
-        current0El.textContent = currentScore //change later 
-    } else {
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }
+    else {
         //switch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
 
 
-function switchPlayer() {
-    if()
-}
+ 
