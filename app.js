4cbac934de22e57c521e21d9738c0bd615b7445f
daudi13 <davouma13@gmail.com>
2021-07-01 06:38:43 +0300

    replace diceEl with diceElement

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-const diceEl = document.querySelector('.dice');
+const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -15,7 +15,7 @@ const btnHold = document.querySelector('.btn--hold');
 //defaults
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+diceElement.classList.add('hidden');
 
 const scores = [0, 0]
 let currentScore = 0;
@@ -36,8 +36,8 @@ btnRoll.addEventListener('click', () => {
 
         //2. display roll dice 
 
-        diceEl.classList.remove('hidden');
-        diceEl.src = `/images/dice-${dice}.png`;
+        diceElement.classList.remove('hidden');
+        diceElement.src = `/images/dice-${dice}.png`;
 
         //3. check for rollers 1: if true, swith to next player
 
@@ -68,7 +68,7 @@ btnHold.addEventListener('click', function () {
             playing = false
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-            diceEl.classList.add('hidden');
+            diceElement.classList.add('hidden');
 
         } else {
             switchPlayer()
