c229365fb2b7f033e20ed8f1bc00f24edcf652d8
daudi13 <davouma13@gmail.com>
2021-07-01 05:28:18 +0300

    commit function

--- app.js
+++ app.js
@@ -13,15 +13,11 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //defaults
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
 
-let playing = true
+let scores,currentScore, activePlayer, playing
+init()
+
 
 //rolling the dice function 
 
@@ -88,4 +84,27 @@ function switchPlayer() {
         activePlayer = activePlayer === 0 ? 1 : 0;
         player0El.classList.toggle('player--active');
         player1El.classList.toggle('player--active');
+}
+
+btnNew.addEventListener('click', function () {
+    init()
+
+})
+
+function init() {
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true
+
+    score1El.textContent = 0;
+    score0El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player-winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
 }
